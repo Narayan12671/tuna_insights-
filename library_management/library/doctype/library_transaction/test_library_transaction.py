e41bc23e74c1cd02abbf78f8da6696e0e699154e
Narayan12671 <narayanpokharel66666@gmail.com>
2024-12-03 12:31:07 +0545

    new app

---
+++ library_management/library/doctype/library_transaction/test_library_transaction.py
@@ -0,0 +1,12 @@
+# Copyright (c) 2024, narayan and Contributors
+# See license.txt
+
+# import frappe
+# from frappe.tests.utils import FrappeTestCase
+# from frappe.model.docstatus import DocStatus
+# from frappe.model.document import Document
+
+
+
+class TestLibraryTransaction(FrappeTestCase):
+    pass
